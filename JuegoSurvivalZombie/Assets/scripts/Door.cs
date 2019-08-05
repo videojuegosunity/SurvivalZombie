e5aec0c8cfdf1a3b747a0cf28b717f7f76ec142f
Sunjing Lama <luislama@espol.edu.ec>
2019-08-04 21:41:44 -0500

    [ADD] Door at the end of the level 1 scene for ending the game.

---
+++ JuegoSurvivalZombie/Assets/scripts/Door.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour
+{
+    public float spriteBlinkingTimer = 0.0f;
+    public float spriteBlinkingMiniDuration = 0.1f;
+    public float spriteBlinkingTotalTimer = 0.0f;
+    public float spriteBlinkingTotalDuration = 1.0f;
+    public bool startBlinking = false;
+    // Start is called before the first frame update
+
+    int count = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        SpriteBlinkingEffect();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision){
+        if (collision.gameObject.tag == "Player"){
+            SceneManager.LoadScene("Nivel2");
+        }
+    }
+
+    private void SpriteBlinkingEffect()
+    {
+        spriteBlinkingTotalTimer += Time.deltaTime;
+        if(spriteBlinkingTotalTimer >= spriteBlinkingTotalDuration)
+        {
+            startBlinking = false;
+            spriteBlinkingTotalTimer = 0.0f;
+            this.gameObject.GetComponent<SpriteRenderer>().enabled = true;   // according to
+                      //your sprite
+            return;
+        }
+
+        spriteBlinkingTimer += Time.deltaTime;
+        if(spriteBlinkingTimer >= spriteBlinkingMiniDuration)
+        {
+            spriteBlinkingTimer = 0.0f;
+            if (this.gameObject.GetComponent<SpriteRenderer>().enabled == true) {
+                this.gameObject.GetComponent<SpriteRenderer>().enabled = false;  //make changes
+            } else {
+                this.gameObject.GetComponent<SpriteRenderer>().enabled = true;   //make changes
+            }
+        }
+    }
+}
