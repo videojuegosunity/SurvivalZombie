1ebdec128efcc390543581a722b11cd4d4645f5c
Nancychalen <Chalen_na201@outlook.com>
2019-08-05 07:42:28 -0500

    menu scene

---
+++ JuegoSurvivalZombie/Assets/Controller.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Controller : MonoBehaviour
+{
+    public void CambiarEscena(string nombre)
+    {
+        SceneManager.LoadScene(nombre);
+    }
+
+    public void Salir()
+    {
+        Debug.Log("QUIT GAME");
+        Application.Quit();
+    }
+}
---
+++ JuegoSurvivalZombie/Assets/dialogue.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class dialogue : MonoBehaviour
+{
+    public Text txt;
+    public string texto;
+
+    // Start is called before the  first frame update
+    void Start()
+    {
+        StartCoroutine(ditado(texto));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    IEnumerator ditado(string frase)
+    {
+        int letra = 0;
+        txt.text = "";
+        while (letra < frase.Length)
+        {
+            txt.text += frase[letra];
+            letra += 1;
+            yield return new WaitForSeconds(0.02f);
+        }
+    }
+}
